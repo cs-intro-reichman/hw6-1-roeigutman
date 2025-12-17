0081f6dc422c1d72513e467cab8e568af34683eb
roei gutman <git config --global user.email david.german@post.runi.ac.il>
2025-12-17 17:02:37 +0200

    Completed assignment

--- Editor4.java
+++ Editor4.java
@@ -1,10 +1,25 @@
 
-
+import java.awt.Color;
+ 
 public class Editor4 {
     
 public static void main (String[] args) {
 
+String source = args[0];
+		String target = args[1];
+		int n = Integer.parseInt(args[2]);
+		Color[][] sourceImage = Runigram.read(source);
+		Color[][] targetImage = Runigram.read(target);
+		   
 
+        Color [][] gray = Runigram.grayScaled(sourceImage);
+
+        Color [][] gray1 = Runigram.grayScaled(targetImage);
+        
+        
+        
+        Runigram.setCanvas(gray);
+		Runigram.morph(gray, gray1, n);
 
 
 }
